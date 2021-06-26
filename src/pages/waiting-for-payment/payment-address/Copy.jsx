84d07d16d798398c08c91a3b454efe57d6ac9518
Sergey Tolstokorov <stolstokorov2020@yandex.ru>
2021-06-27 05:03:17 +0700

    .

--- src/pages/waiting-for-payment/payment-address/Copy.jsx
+++ src/pages/waiting-for-payment/payment-address/Copy.jsx
@@ -3,10 +3,10 @@ import st from './Copy.module.scss';
 
 function Copy({ placeholder, value = '' }) {
     const [inputValue, setInputValue] = useState(value);
-    const [success, setSuccess] = useState();
+    const [success, setSuccess] = useState(false);
     return (
         <div className={ st['wrapper'] }>
-            <textarea disabled className={ st['textarea'] } type="text" 
+            <textarea className={ st['textarea'] } type="text" 
                 placeholder={ placeholder }
                 value={ inputValue }
                 onChange={ (e) => setInputValue(e.target.value) }/>
