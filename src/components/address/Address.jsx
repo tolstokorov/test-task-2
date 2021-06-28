3d5bec7a6f83759613fe17ac9d5aad2e9b3db467
Sergey Tolstokorov <stolstokorov2020@yandex.ru>
2021-06-28 17:36:42 +0700

    сору / paste

--- src/components/address/Address.jsx
+++ src/components/address/Address.jsx
@@ -1,11 +1,17 @@
+import { useState } from 'react';
 import st from './Address.module.scss';
 
 function Address({ placeholder, rightText }) {
+  const [value, setValue] = useState('');
   return (
     <div className={ st['address'] }>
         <input className={ st['input'] }
-        placeholder={ placeholder } />
-        <span className={ st['right-text'] }>{rightText}</span>
+        placeholder={ placeholder } value={ value }/>
+        <button onClick={ () => {
+          navigator.clipboard.readText()
+            .then(text => setValue(text));
+        }}
+          className={ st['right-text'] }>{rightText}</button>
     </div>
   );
 }
--- src/pages/waiting-for-payment/payment-address/Copy.jsx
+++ src/pages/waiting-for-payment/payment-address/Copy.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { createRef, useState } from 'react';
 import st from './Copy.module.scss';
 
 function Copy({ placeholder, value = '' }) {
@@ -10,7 +10,10 @@ function Copy({ placeholder, value = '' }) {
                 placeholder={ placeholder }
                 value={ inputValue }
                 onChange={ (e) => setInputValue(e.target.value) }/>
-            <button onClick={ () => setSuccess(true) } className={ st['btn'] }>Copy</button>
+            <button onClick={ () => {
+                navigator.clipboard.writeText(inputValue);
+                setSuccess(true);
+            } } className={ st['btn'] }>Copy</button>
             { success && <div className={ st['success'] }>Код успешно скопирован!</div> }
         </div>
     );
